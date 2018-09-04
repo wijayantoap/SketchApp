c7320b2d6cd9d1a0f707957d66eb116ed50b5d77
Wijayanto A.P <wijayanto.a.p@gmail.com>
2018-09-05 03:22:25 +0800

    canvas and color picker

--- App.js
+++ App.js
@@ -5,20 +5,77 @@
  * @format
  * @flow
  */
-
+'use strict'
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button, Alert, Dimensions} from 'react-native';
 import { SketchCanvas } from '\@terrylinla/react-native-sketch-canvas';
+import { ColorPicker, toHsv } from 'react-native-color-picker'
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor() {
+    super();
+    this.state = {
+      strokeColor : "blue",
+      color: toHsv('blue'),
+      isHidden : false,
+      buttonText : 'Change Color',
+    }
+    this._onColorChange = this._onColorChange.bind(this)
+  }
+
+  _onColorChange(color) {
+    this.setState({ color })
+  }
+
+  _showPicker = () => {
+    this.setState({isHidden:!this.state.isHidden});
+    if (this.state.isHidden) {
+      this.setState({buttonText : 'Change color'}); 
+    } else {
+      this.setState({buttonText : 'Close picker'}); 
+    }
+  }
+
+  _renderCancel() {
+    if (this.state.isHidden) {
+      return (
+          <ColorPicker
+            color={this.state.color}
+            defaultColor={this._onColorChange}
+            onColorChange={this._onColorChange}
+            onColorSelected={color => this.setState({strokeColor : color})}
+            style={{width:250, height:250}}
+          />
+      )
+      
+    } else {
+      return null;
+      this.setState({buttonText : 'ASD'}); 
+    }
+  }
+  
   render() {
     return (
       <View style={styles.container}>
+        <Button
+            onPress={this._showPicker}
+            title={this.state.buttonText}
+            color={this.state.strokeColor}
+          ></Button>
+        {this._renderCancel()}
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            alignSelf: 'stretch'
+          }}
+        />
         <View style= {{ flex: 1, flexDirection: 'row'}}>
           <SketchCanvas
           style={{ flex: 1}}
-          strokeColor={'red'}
+          strokeColor={this.state.strokeColor}
           strokeWidth={7}
           />
         </View>
@@ -33,5 +90,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  hidden: {
+    width: 0,
+    height: 0,
   },
 });
